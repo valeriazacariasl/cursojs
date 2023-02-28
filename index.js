eb74a799495b3211ed9632171de66b21ba0b74d3
Valeria Zacarías <vall.zacarias26@gmail.com>
2023-02-28 11:37:24 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* Operadores matematicos
+let a = 5;
+let b = 2;
+
+let usuario = "Valeria";
+
+console.log(a-b);
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log("Hola " + usuario); */
+
+/* Operadores Incrementos - Decremento */
+
+let a = 5;
+
+// console.log(a);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+console.log(a--);
+console.log(a);
+console.log(--a);
